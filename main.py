a7cb06f1233cc4b8283ab0d1759daf8f5ee3308c
1058910 <31193963-1058910@users.noreply.replit.com>
2024-01-13 01:24:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to the YouTube Channel Name Generator: ")
+name = input("What is your nickname? \n")
+topic = input("What is your channel about? \n")
+print ("You could name your channel " + "(" + topic + " with " + name + ")")
